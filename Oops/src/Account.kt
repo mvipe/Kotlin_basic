782230c1c7747fb794936920a3022eeefd204b68
mvipe <mohnishvats@gmail.com>
2021-12-26 18:35:41 +0530

    Working with oops

--- Oops/src/Account.kt
+++ Oops/src/Account.kt
@@ -1,2 +1,15 @@
+fun main(){
+    var acc=Account("jjkkkk")
+}
+
 class Account {
+    var accNo:Int
+    var name:String
+
+    //secondary constructor
+    constructor(name: String)  {
+        this.name=name
+        this.accNo=0
+
+    }
 }
--- Oops/src/User.kt
+++ Oops/src/User.kt
@@ -1,13 +1,33 @@
 fun main(){
-    val user:User=User()
+    val user:User=User("raj","raj@gmail.com",20)
     println(user.age)
 }
 
 
-class User {
-    val name:String=""
-    val email:String=""
-    val age =0
+class User(name:String,email:String,age:Int=0) {
+    var name:String
+    var email:String
+    var age :Int
+
+    //constructor using init block
+    //primary constructor
+    init {
+        this.name=name
+        this.email=email
+        this.age=age
+    }
+
+
+
+
+    /*
+        It is a function with same name as class name
+        It is used to construct or Initialize the class
+        Two types of constructor
+        1.Primary Constructor
+        2.Secondary Constructor
+     */
+
 
     fun checkEmail():Boolean{
         return email.isNotEmpty()
